cdb26893282ad8d3bc7e18723f377ef6eff13a62
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-08 14:32:29 +0400

    Time: 337 ms (38.72%), Space: 76.7 MB (85.95%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+         int n = position.size();
+        if (n == 1) return 1;
+        
+        vector<pair<int, double>> startPosAndTravelTime(n);
+        for (int i = 0; i <n;  i++)
+        {
+            startPosAndTravelTime[i] = {position[i], ((double)target - position[i])/speed[i]};
+        }
+        sort(startPosAndTravelTime.begin(), startPosAndTravelTime.end());
+        
+        double currTime = startPosAndTravelTime[n-1].second;
+        int ret = 1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (startPosAndTravelTime[i].second <= currTime)
+                continue;
+            ret++;
+            currTime = startPosAndTravelTime[i].second;
+        }
+        
+        return ret;
+    }
+};
