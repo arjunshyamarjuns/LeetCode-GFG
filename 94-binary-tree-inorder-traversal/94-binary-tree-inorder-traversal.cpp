754c18fbbcfb362c70fda8a29d5822809356ac26
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-16 09:05:10 +0400

    Time: 0 ms (100.00%), Space: 8.3 MB (75.74%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -15,15 +15,17 @@ public:
          vector<int>ans;
         stack<TreeNode* >st;
         TreeNode* node = root;
-        while(true) {
-            if(node != nullptr) {
+        while(node!=NULL || !st.empty()) {
+            if(node != NULL) 
+            {
                 st.push(node);
                 node = node->left;
-            } else {
-                if(st.empty()) break;
+            }
+            else
+            {
                 node = st.top();
-                st.pop();
                 ans.push_back(node->val);
+                st.pop();
                 node = node->right;
             }
         }
