5cefce3f76db122588627c6ed4d18843a8c3e14a
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-20 14:29:08 +0400

    Time: 47 ms (60.45%), Space: 12.3 MB (84.22%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-   void recur(vector<vector<char>> &grid,int i,int j){
-        grid[i][j]='0';
-        if(i-1>=0 && grid[i-1][j]=='1'){
-            recur(grid,i-1,j);
-        }
-        if(i+1<grid.size() && grid[i+1][j]=='1'){
-            recur(grid,i+1,j);
-        }
-        if(j-1>=0 && grid[i][j-1]=='1'){
-            recur(grid,i,j-1);
-        }
-        if(j+1<grid[0].size() && grid[i][j+1]=='1'){
-            recur(grid,i,j+1);
-        }
-    }
+   void recur(vector<vector<char>> &grid,int x,int y,int r,int c){
+       if(x<0||x>=r||y<0||y>=c||grid[x][y]!='1')
+           return;
+       grid[x][y]='2';
+       recur(grid,x+1,y,r,c);
+       recur(grid,x-1,y,r,c);
+       recur(grid,x,y+1,r,c);
+       recur(grid,x,y-1,r,c);
+   }
     int numIslands(vector<vector<char>>& grid) {
-        int res=0;
+        int row=grid.size();
+        int col=grid[0].size();
+        int no_of_islands=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
-                    res++;
-                    recur(grid,i,j);
+                    recur(grid,i,j,row,col);
+                    no_of_islands++;
                 }
             }
         }
-        return res;
+        return no_of_islands;
     }
 };
