4de1128b2e1d60088ce26ae300f7611394064bc4
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-22 09:15:12 +0400

    Time: 41 ms (32.48%), Space: 11.9 MB (70.62%) - LeetHub

--- 529-minesweeper/529-minesweeper.cpp
+++ 529-minesweeper/529-minesweeper.cpp
@@ -1,45 +1,68 @@
 class Solution {
 public:
-    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
-        int row = click[0], col = click[1];
-        if(board[row][col] == 'M') board[row][col] = 'X';
-        else revealAllPos(board, row, col);
-        return board;
+    int pqr(vector<vector<char>>& board, int x,int y){
+        if(x<0||x>=board.size()||y<0||y>=board[0].size()){
+            return 0;
+        }
+        if(board[x][y]=='M'){
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
     
-private:
-    int directions[8][2] = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
     
-    void revealAllPos(vector<vector<char>>& board, int row, int col){
-       int m = board.size(), n= board[0].size(), mineCount = 0;
-	   
-        //Check whether mine exists or not in all 8 valid cells
-        for(int i=0; i<8; i++){
-            int newRow = directions[i][0] + row;
-            int newCol = directions[i][1] + col;
-            
-            if(isValid(newRow, newCol, m, n) && board[newRow][newCol] == 'M') mineCount += 1;
-        }
+    
+    int xyz(vector<vector<char>>& board, int x,int y){
+        return pqr(board,x-1,y-1)+pqr(board,x,y-1)+pqr(board,x-1,y)+pqr(board,x-1,y+1)+pqr(board,x+1,y-1)+pqr(board,x+1,y+1)+pqr(board,x+1,y)+pqr(board,x,y+1);
         
-        //Reveal adjacent cells if current cell is 'B'
-        if(mineCount == 0) {
-            board[row][col] = 'B';
-            
-            for(int i=0; i<8; i++){
-                int newRow = directions[i][0] + row;
-                int newCol = directions[i][1] + col;
-
-                if(isValid(newRow, newCol, m, n) && board[newRow][newCol] == 'E') {
-                    revealAllPos(board, newRow, newCol);
-                }
-            }
-        }
-        else board[row][col] = mineCount + 48;
     }
     
     
-    bool isValid(int row, int col, int totalRows, int totalCols){
-        if(row >= 0 && col >= 0 && row < totalRows && col < totalCols) return true;
-        return false;
+    
+    void abc(vector<vector<char>>& board,int x,int y) {
+        if(x<0||x>=board.size()||y<0||y>=board[0].size()){
+            return;
+            
+        }
+        if(board[x][y]=='M'){
+            board[x][y]='X';
+            return;
+            
+        }
+         if(board[x][y]=='X'||board[x][y]=='B'||(board[x][y]>='1'&&board[x][y]<='9')){
+            
+            return;
+            
+        }
+        if(board[x][y]=='E'){
+            if(xyz(board,x,y)>=1){
+                board[x][y]='0'+xyz(board,x,y);
+            return;
+                
+            }
+            else{
+                 board[x][y]='B';
+                abc(board,x-1,y-1);
+                abc(board,x,y-1);
+                abc(board,x-1,y);
+                abc(board,x-1,y+1);
+                abc(board,x+1,y-1);
+                abc(board,x+1,y+1);
+                abc(board,x+1,y);
+                abc(board,x,y+1);
+                return;
+                
+                
+            }
+            
+        }
+    }
+        
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        abc(board,click[0],click[1]);
+        return board;
+      
     }
 };
