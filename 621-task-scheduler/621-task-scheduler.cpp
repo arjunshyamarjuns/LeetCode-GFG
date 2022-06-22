378cdf2b5b6aba24c8cf12fbfa19bf1f7d0c493f
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-22 14:24:05 +0400

    Time: 191 ms (20.70%), Space: 43.8 MB (24.05%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+         unordered_map<char,int> mp;  
+        priority_queue<int> pq;
+        for(auto c : tasks)
+            mp[c]++;
+        for(auto it : mp) 
+            pq.push(it.second);
+        int ans = 0;
+        int cycle = n+1;
+        while(!pq.empty()){
+            int time = 0;
+            vector<int> temp;
+            for(int i=0 ; i<cycle ; ++i)
+            {
+                if(!pq.empty())
+                {
+                    temp.push_back(pq.top());
+                    pq.pop();
+                    time++;
+                }
+            }
+            for(int i : temp) if(--i) 
+                pq.push(i);
+            ans += (!pq.empty()) ? cycle : time;
+        }
+        return ans;
+    }
+};
