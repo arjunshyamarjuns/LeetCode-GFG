dedb62c6ab9f750e47577b1cea68e03943883fed
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-28 08:33:51 +0400

    Time: 3 ms (56.90%), Space: 7.2 MB (50.04%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -1,78 +1,32 @@
-class Tweet{
-    public:
+class Twitter {
+
+   vector<pair<int, int>> whoPosted;
+    unordered_map<int, unordered_set<int>> whoFollowWho;
+public:
+    Twitter() {
+    }
     
-    int userId;
-    int tweetId;
-    int time;
-    Tweet(int userId,int tweetId,int time)
-    {
-        this->userId=userId;
-        this->tweetId=tweetId;
-        this->time=time;
+    void postTweet(int userId, int tweetId) {
+        whoPosted.push_back({ userId, tweetId });
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        unordered_set<int> s = whoFollowWho[userId];
+        s.insert(userId);
+        vector<int> ans;
+        for (int i = whoPosted.size() - 1; i > -1 && ans.size() < 10; --i) if (s.find(whoPosted[i].first) != s.end()) ans.push_back(whoPosted[i].second);
+        return ans;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        whoFollowWho[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        whoFollowWho[followerId].erase(followeeId);
     }
 };
 
-class Twitter {
-public:
-    
-    map<int,set<pair<int,Tweet*>>>userFeed;
-    map<int,set<Tweet*>>userPost;
-    map<int,set<int>>myFollowers;
-    int time;
-    
-    Twitter() 
-    {
-        time=1e9;
-        userFeed.clear();
-        myFollowers.clear();
-    }
-    
-    void postTweet(int userId, int tweetId) 
-    {
-       Tweet* newTweet = new Tweet(userId,tweetId,time);
-       userFeed[userId].insert({time,newTweet});
-       userPost[userId].insert(newTweet);
-       time--;
-        
-       for(auto i : myFollowers[userId])
-       {
-           userFeed[i].insert({newTweet->time,newTweet});
-       }
-    }
-    
-    vector<int> getNewsFeed(int userId) 
-    {
-        vector<int>v;
-        for(auto i : userFeed[userId])
-        {
-            v.push_back(i.second->tweetId);
-            if(v.size()==10)
-            {
-                break;
-            }
-        }
-        
-        return v;
-    }
-    
-    void follow(int followerId, int followeeId) 
-    {
-        myFollowers[followeeId].insert(followerId);
-        for(auto it : userPost[followeeId])
-        {
-            userFeed[followerId].insert({it->time,it});
-        }
-    }
-    
-    void unfollow(int followerId, int followeeId) 
-    {
-        myFollowers[followeeId].erase(followerId);
-        for(auto it : userPost[followeeId])
-        {
-            userFeed[followerId].erase({it->time,it});
-        }
-    }
-};
 /**
  * Your Twitter object will be instantiated and called as such:
  * Twitter* obj = new Twitter();
