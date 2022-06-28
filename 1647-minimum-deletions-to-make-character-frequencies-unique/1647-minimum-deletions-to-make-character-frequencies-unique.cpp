e47d0af9000264646e0c79de310459d94f94cc10
Arjun shyam <63267752+arjunshyamarjuns@users.noreply.github.com>
2022-06-28 09:45:37 +0400

    Time: 91 ms (53.01%), Space: 17.5 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int minDeletions(string s) {
-       unordered_map<char,int>mpp;
-        for(int i = 0;i < s.length();i++){
-            mpp[s[i]]++;
-        }
-        set<int>st;
-        int count = 0;
-        // for(auto it : mpp){
-        //     cout << it.first << " : " << it.second << endl;
-        // }
-        for(auto it : mpp){
-            if(st.find(it.second) == st.end()){
-                st.insert(it.second);
-            }
-            else{
-                while(st.find(it.second) != st.end() && it.second != 0){
-                    it.second--;
-                    count++;
-                }
-                if(it.second != 0){
-                    st.insert(it.second);
-                }
-            }
+      int freq[26]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s.at(i)-'a']++;
             
         }
-        return count;
-        
+        unordered_set<int> occupiedfreq;
+        int deletions=0;
+        for(int i=0;i<26;i++)
+        {
+            int currfreq=freq[i];
+            while(currfreq>0)
+            {
+                if(occupiedfreq.find(currfreq)!=occupiedfreq.end())
+                    {
+                        currfreq--;
+                        deletions++;
+                    }
+                else
+                    {
+                        break;
+                    }
+            }
+            if(currfreq!=0)
+                occupiedfreq.insert(currfreq);
+        }
+        return deletions;
     }
 };
